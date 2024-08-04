16108bbea23cd2c8b40eb0a7ea2a474ac8a905dc
proofone <proof87@gmail.com>
2024-08-04 22:39:04 +0200

    Ref for variables

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -21,27 +21,53 @@
 
       </div>
     </div>
+    <h3>{{ title }}</h3>
+    <p><q-toggle size="lg" v-model="gaVal" label="GUEST ALLOWS US TO GET IN CONTACT BY EMAIL" /></p>
+    <div class="row">
+      <div class="col-6">
+        <div class="row">
+
+          <div class="col-3">LAST NAME:</div>
+          <div class="col-3">{{ guestData?.LAST_NAME }}</div>
+          <div class="col-3">EMAIL ADDRESS:</div>
+          <div class="col-3">{{ guestData?.EMAIL_ADDRESS_ }}</div>
+          <div class="col-3">FIRST NAME:</div>
+          <div class="col-3">{{ guestData?.FIRST_NAME }}</div>
+          <div class="col-3">EMAIL STATUS:</div>
+          <div class="col-3">{{ guestData?.EMAIL_DELIVERABILITY_STATUS_ }}</div>
+        </div>
+
+      </div>
+      <div class="col-6">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import { Meta, GuestData } from './models';
+import { Meta, GuestData } from './models';
 
 interface Props {
+  title?: string;
   title?: string;
   meta: Meta;
   active: boolean;
   guestAllows?: boolean
   guestData?: GuestData
+  guestAllows?: boolean
+  guestData?: GuestData
 };
 
-const props = withDefaults(defineProps<Props>(), {
-  title: () => gaVal ? "Guest Consent" : "Guest does not consent",
+const props = ref({
+  guestData: {},
   guestAllows: () => false,
 });
 
-const gaVal = false
+let gaVal = ref(false)
+const title = computed(() => gaVal ? "Guest Consent" : "Guest does not consent")
 //const guestAllowsChange = (event) => {props.guestAllows = e.target.value}
 const clickCount = ref(0);
 function increment() {
@@ -49,5 +75,6 @@ function increment() {
   return clickCount.value;
 }
 
+//const todoCount = computed(() => props.todos.length);
 //const todoCount = computed(() => props.todos.length);
 </script>
